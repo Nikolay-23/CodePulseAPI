fc759fbad6242aec32b2529a142b975bc89abbba
Nikolay-23 <115502655+Nikolay-23@users.noreply.github.com>
2025-06-29 23:52:10 +0000

    Change on ImagesController

---
+++ Models/DTO/UploadImageRequest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CodePulseAPI.Models.DTO
+{
+    public class UploadImageRequest
+    {
+        public IFormFile File { get; set; } = null!;
+        public string FileName { get; set; } = null!;
+        public string Title { get; set; } = null!;
+    }
+}
